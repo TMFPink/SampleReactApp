f024b01751da122d18b8991f20e00093ed0c3b93
TMFPink <nguyenhongtranminh@gmail.com>
2024-09-15 10:46:14 +0700

    hello world in react app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div class='items-center flex justify-center content-center bg-gradient-to-r from-blue-700 to-zinc-200 h-screen'>
+      <div class='flex flex-col'>
+        <h1 class='text-7xl font-bold'>Welcome to our React App</h1>
+        <div class='items-center flex justify-center content-center flex-col mt-8'>
+          <h1 class='text-3xl font-light'>Tran Minh Nguyen Hong</h1>
+          <h1 class='text-3xl font-light'>Nguyen Toan Khang</h1>
+        </div>
+      </div>
     </div>
   );
 }
